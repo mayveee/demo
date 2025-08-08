8bb4e2fce96318434c029a7dda5c4db7fce10f90
admin <admin@DESKTOP-75DAID8>
2025-08-08 11:16:52 +0900

    main 수정

---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+// src/pages/Main.tsx
+import React from "react";
+import "./Main.css";
+
+export default function Main() {
+  return (
+    <div className="full">
+      <button className="glass-btn">✨ Click Me</button>
+    </div>
+  );
+}
